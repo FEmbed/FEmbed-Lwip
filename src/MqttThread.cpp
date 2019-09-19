bc0e3b052bac0be3bcbaef512acf8d7f345dc8ea
yunxiang.gong <gyx_edu@qq.com>
2019-09-19 21:42:33 +0800

    1. Mqtt OSTask

---
+++ src/MqttThread.cpp
@@ -0,0 +1,23 @@
+/*
+ * MqttThread.cpp
+ *
+ *  Created on: 2019年9月19日
+ *      Author: Gene Kong
+ */
+
+#include "MqttThread.h"
+
+namespace FEmbed {
+
+MqttThread::MqttThread()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+MqttThread::~MqttThread()
+{
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace FEmbed */
---
+++ src/MqttThread.h
@@ -0,0 +1,22 @@
+/*
+ * MqttThread.h
+ *
+ *  Created on: 2019年9月19日
+ *      Author: Gene Kong
+ */
+
+#ifndef FEMBED_LWIP_SRC_MQTTTHREAD_H_
+#define FEMBED_LWIP_SRC_MQTTTHREAD_H_
+
+namespace FEmbed {
+
+class MqttThread
+{
+ public:
+    MqttThread();
+    virtual ~MqttThread();
+};
+
+} /* namespace FEmbed */
+
+#endif /* FEMBED_LWIP_SRC_MQTTTHREAD_H_ */
