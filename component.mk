97ff70786907fa9a6d81e3247f64c4b8bfb52da9
yunxiang.gong <gyx_edu@qq.com>
2019-09-20 15:54:45 +0800

    1. update for lwip wrap

--- component.mk
+++ component.mk
@@ -1,5 +1,11 @@
 #
 # Component Makefile
 #
-COMPONENT_ADD_INCLUDEDIRS += src
-COMPONENT_SRCDIRS += src
+COMPONENT_ADD_INCLUDEDIRS += paho/MQTTClient/src \
+                             paho/MQTTPacket/src \
+                             src
+
+COMPONENT_SRCDIRS += paho/MQTTClient/src \
+                     paho/MQTTPacket/src \
+                     src
+
