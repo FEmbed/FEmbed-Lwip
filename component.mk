449836b33e1e66911d20d5c8a5d8aa4fc07274f6
yunxiang.gong <gyx_edu@qq.com>
2019-09-29 17:20:56 +0800

    1. update for FEmbed compile

--- component.mk
+++ component.mk
@@ -9,3 +9,6 @@ COMPONENT_SRCDIRS += paho/MQTTClient/src \
                      paho/MQTTPacket/src \
                      src
 
+ifndef CONFIG_MBEDTLS
+COMPONENT_OBJEXCLUDE := src/TCPClientSecure.o src/ssl_client.o
+endif
